5a86c2c576b2ab6fdaa652544101b62437bddac2
Syahrulramadhan <syahrulramakoto55@gmail.com>
2024-06-24 01:49:24 +0700

    UAS

---
+++ database/migrations/2024_06_23_181615_create_status_pasiens_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('status_pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pasien_id')->constrained('data_pasiens')->onDelete('cascade');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('status_pasiens');
+    }
+};
